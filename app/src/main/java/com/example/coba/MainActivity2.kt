d9380578d344a102daf1fc4bf8cc4b79f5211cff
NASHWA HARZATHI RAHMA <162123885+Nashwahrz@users.noreply.github.com>
2024-08-31 22:54:00 +0700

    tugas 3 dan kalkulator

--- app/src/main/java/com/example/coba/MainActivity2.kt
+++ app/src/main/java/com/example/coba/MainActivity2.kt
@@ -1,16 +1,30 @@
 package com.example.coba
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity2 : AppCompatActivity() {
+
+    lateinit var kalkulator: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main2)
+        kalkulator = findViewById(R.id.kalkulator)
+
+        kalkulator.setOnClickListener() {
+            val intent = Intent(this, MainActivity3::class.java)
+            startActivity(intent)
+        }
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
